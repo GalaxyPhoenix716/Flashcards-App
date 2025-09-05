d895c851ac0e7e92f3ddd184dff509d66c871ca5
GalaxyPhoenix <galaxyphoenix716@gmail.com>
2025-09-05 16:38:33 +0530

    created card tile for catgory screen

--- lib/screens/category_screen/view/home_screen.dart
+++ lib/screens/category_screen/view/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:flashcards_app/screens/category_screen/widgets/create_new_set_bu
 import 'package:flashcards_app/screens/category_screen/widgets/curved_edges.dart';
 import 'package:flashcards_app/screens/category_screen/widgets/favourite_cards_button.dart';
 import 'package:flashcards_app/utils/colors.dart';
+import 'package:percent_indicator/flutter_percent_indicator.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -30,6 +31,7 @@ class _HomeScreenState extends State<HomeScreen>
   Widget build(BuildContext context) {
     int noOfCards = 10;
     return Scaffold(
+      backgroundColor: FcColors.white,
       body: Column(
         children: [
           ClipPath(
@@ -98,6 +100,94 @@ class _HomeScreenState extends State<HomeScreen>
                     ],
                   ),
                 ],
+              ),
+            ),
+          ),
+
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: FcColors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withValues(alpha: 0.15),
+                          blurRadius: 10,
+                          offset: const Offset(0, 5),
+                        ),
+                      ],
+                    ),
+                    child: Stack(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(25),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 10),
+                                    child: Text(
+                                      "Nouns",
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 8),
+                                  LinearPercentIndicator(
+                                    animation: true,
+                                    animationDuration: 500,
+                                    width: 140,
+                                    lineHeight: 8,
+                                    percent: 0.7,
+                                    backgroundColor: FcColors.grey,
+                                    progressColor: FcColors.progress,
+                                    barRadius: Radius.circular(5),
+                                  ),
+                                ],
+                              ),
+                          
+                              Column(
+                                children: [
+                                  const SizedBox(height: 15),
+                                  FittedBox(
+                                    child: Text(
+                                      "15/150",
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+
+                        Positioned(
+                          top: -5,
+                          right: -5,
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.more_vert, color: FcColors.grey),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                itemCount: 10,
               ),
             ),
           ),
